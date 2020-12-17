e521c571cbf9e948c7c43202e73f2aa05491d8ab
firelemons <shenyang64@gmail.com>
2020-12-17 04:07:11 -0600

    send me to the glue facotry

--- 06_handy_haversacks/handy_haversacks.py
+++ 06_handy_haversacks/handy_haversacks.py
@@ -1,44 +1,53 @@
 #!/usr/bin/python3
 import re
-import pprint
 
-input_bag_data = open("input_simple.txt", "r")
+input_bag_data = open("input.txt", "r")
 
 bags_data = {}
 valid_bag_color_count = 0
 
-line_lexer = re.compile(r"(\w+\ \w+)\ bags\ contain\ (\d+|no)\ ((\w+\ \w+)\ bags?|other\ bags)(,\ (\d+)\ (\w+\ \w+))?")
+color_lexer = re.compile(r"^(\w+\ \w+)")
+contents_lexer = re.compile(r"(\d+)\ (\w+\ \w+)\ bags?")
 
 for line in input_bag_data:
-    line_data = line_lexer.match(line)
-    if line_data.group(4):
-        bag_color = line_data.group(1)
-        minmum_child_quantity = int(line_data.group(2))
-        child_color = line_data.group(4)
+    bag_contents = contents_lexer.findall(line)
+    if len(bag_contents):
+        bag_color = color_lexer.match(line).group(1)
+        for bag_spec in bag_contents:
+            minmum_child_quantity = int(bag_spec[0])
+            child_color = bag_spec[1]
 
-        if not bag_color in bags_data:
-            bags_data[bag_color] = {}
+            if not bag_color in bags_data:
+                bags_data[bag_color] = {}
 
-        bag_data = bags_data[bag_color]
+            bag_data = bags_data[bag_color]
 
-        if not "contents" in bag_data:
-            bag_data["contents"] = {}
+            if not "contents" in bag_data:
+                bag_data["contents"] = []
 
-        bag_data["contents"][child_color] = minmum_child_quantity
+            bag_data["contents"].append(child_color)
 
-        if line_data.group(5):
-            quantity = int(line_data.group(6))
-            color = line_data.group(7)
+            if not child_color in bags_data:
+                bags_data[child_color] = {}
 
-            if not color in bags_data:
-                bags_data[color] = {}
+            child_bag = bags_data[child_color]
 
-            bag_prime_data = bags_data[color]
-
-            if not quantity in bag_prime_data:
-                bag_prime_data["quantity"] = quantity
+            if not "required" in child_bag:
+                child_bag["required"] = [[minmum_child_quantity, bag_color]]
             else:
-                bag_prime_data["quantity"] += quantity
-        
-pprint.pprint(bags_data)
+                child_bag["required"].append([minmum_child_quantity, bag_color])
+
+gold_tree_bfs = [bags_data["shiny gold"]]
+
+while len(gold_tree_bfs):
+    node = gold_tree_bfs.pop()
+
+    if "required" in node:
+        for color in node["required"]:
+            color_name = color[1]
+            if not "is_member" in bags_data[color_name]:
+                bags_data[color_name]["is_member"] = True
+                valid_bag_color_count += 1
+                gold_tree_bfs.append(bags_data[color_name])
+
 print(valid_bag_color_count)
