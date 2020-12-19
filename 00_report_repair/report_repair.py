53a053c107e1b511215ef13e8afb2f862ed2c46c
firelemons <shenyang64@gmail.com>
2020-12-19 04:02:14 -0600

    python3 and stuff

--- 00_report_repair/report_repair.py
+++ 00_report_repair/report_repair.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import bisect
+import math
 
 input_numbers = open("input.txt", "r")
 numbers = []
@@ -9,6 +10,8 @@ for x in input_numbers:
 end = len(numbers) - 1
 i = 0
 
+midpoint = lambda search_end, search_start: math.floor(((search_end - search_start) / 2) + search_start)
+
 while i < end:
     # Remove high numbers that can't possibly add to 2020
     while numbers[i] + numbers[end] > 2020:
@@ -17,17 +20,17 @@ while i < end:
     # Binary Search for the 2020 compliment of numbers[i]
     search_start = i + 1
     search_end = end
-    midpoint = ((search_end - search_start) / 2) + search_start
+    mid = midpoint(search_end, search_start)
 
     while search_start <= search_end:
-        if numbers[i] + numbers[midpoint] == 2020:
-            print( numbers[i] * numbers[midpoint] )
+        if numbers[i] + numbers[mid] == 2020:
+            print( numbers[i] * numbers[mid] )
             exit(0)
-        elif numbers[i] + numbers[midpoint] > 2020:
-            search_end = midpoint - 1
+        elif numbers[i] + numbers[mid] > 2020:
+            search_end = mid - 1
         else:
-            search_start = midpoint + 1
+            search_start = mid + 1
       
-        midpoint = ((search_end - search_start) / 2) + search_start
+        mid = midpoint(search_end, search_start)
 
     i += 1
